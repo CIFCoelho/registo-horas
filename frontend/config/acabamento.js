41aae3a129b303985a1abdfc5f95bc511eae9d79
Francisco Coelho <coelho1@ua.pt>
2025-06-12 16:22:01 +0100

    Added initial skeleton for acabamento.js & estofagem.js. Early step to move the inline JS logic to each section specific configuration files

---
+++ frontend/config/acabamento.js
@@ -0,0 +1,17 @@
+window.SECTION_CONFIG = {
+  /* Name sent to the column: “Secção” */
+  section: 'Acabamento',
+
+  /* URL AppsScript Update */
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbxf1CJR2OG2Io90pVLmDqHPc34dj6bicxUQtL48vKVaUlsua2OZZQsoDM7uXpgjQbL5/exec',
+
+  sheetName: 'Acabamento',
+
+  /* Buttons for each person */
+  names: ['Cristina', 'Teresa', 'Sónia', 'Inês', 'Diana', 'Antónia'],
+
+  /* Break Periods */
+  breakPeriods: [
+    { start: '10:00', end: '10:10' },
+  ]
+};
---
+++ frontend/config/estofagem.js
@@ -0,0 +1,17 @@
+window.SECTION_CONFIG = {
+  /* Name sent to the column: "Estofagem - Tempo" */
+  section: 'Estofagem - Tempo',
+
+  /* URL AppsScript Update */
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbxf1CJR2OG2Io90pVLmDqHPc34dj6bicxUQtL48vKVaUlsua2OZZQsoDM7uXpgjQbL5/exec',
+  
+  sheetName: 'Estofagem',
+
+  /* Buttons for each person */  
+  names: ['Antónia', 'Diana', 'Teresa'],
+
+  /* Break Periods */
+  breakPeriods: [
+    { start: '10:00', end: '10:10' },
+  ]
+};
