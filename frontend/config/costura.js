d28fd9cc20574b9b7e888c59f2e34e290255b5dc
Francisco Coelho <coelho1@ua.pt>
2025-06-18 11:42:41 +0100

    Added 'costura' and 'pintura' buttons to the redirect html file

---
+++ frontend/config/costura.js
@@ -0,0 +1,17 @@
+window.SECTION_CONFIG = {
+  /* Name sent to the column: “Secção” */
+  section: 'Costura',
+
+  /* URL AppsScript Update */
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbxf1CJR2OG2Io90pVLmDqHPc34dj6bicxUQtL48vKVaUlsua2OZZQsoDM7uXpgjQbL5/exec',
+
+  sheetName: 'Costura',
+
+  /* Buttons for each person */
+  names: ['Diana', 'Sónia'],
+
+  /* Break Periods */
+  breakPeriods: [
+    { start: '10:00', end: '10:10' },
+  ]
+};
