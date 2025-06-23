387c49c6bcede76ba72e8d87b544c13e3e516e5a
Francisco Coelho <coelho1@ua.pt>
2025-06-23 14:35:21 +0100

    Added .config.js specific file to manage acabamento

---
+++ frontend/JS/config/acabamento.config.js
@@ -0,0 +1,5 @@
+window.SECTION_CONFIG = {
+  section: 'Acabamento',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbxf1CJR2OG2Io90pVLmDqHPc34dj6bicxUQtL48vKVaUlsua2OZZQsoDM7uXpgjQbL5/exec',
+  names: ['Cristina', 'Teresa', 'Sónia', 'Inês', 'Diana', 'Antónia']
+};
