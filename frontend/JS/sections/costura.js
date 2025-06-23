8a66aeed1e123865a650181b9bd824b99bcc092e
Francisco Coelho <coelho1@ua.pt>
2025-06-23 13:30:03 +0100

    Updated costura logic to fit new structure

--- frontend/JS/sections/costura.js
+++ frontend/JS/sections/costura.js
@@ -1,10 +1,6 @@
 window.SECTION_CONFIG = {
-  /* Name sent to the column: “Secção” */
   section: 'Costura',
-
-  /* URL AppsScript Update (DB) */
   webAppUrl: 'https://script.google.com/macros/s/AKfycbxf1CJR2OG2Io90pVLmDqHPc34dj6bicxUQtL48vKVaUlsua2OZZQsoDM7uXpgjQbL5/exec',
-
   sheetName: 'Costura',
 
   /* Buttons for each person */
