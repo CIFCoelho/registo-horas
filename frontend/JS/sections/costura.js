8c2444817b26ee723d6be7bac69740c53466a61b
Francisco Coelho <coelho1@ua.pt>
2025-06-23 13:40:56 +0100

    Changed the idle button color from blue to green

--- frontend/JS/sections/costura.js
+++ frontend/JS/sections/costura.js
@@ -1,6 +1,8 @@
 window.SECTION_CONFIG = {
   section: 'Costura',
+
   webAppUrl: 'https://script.google.com/macros/s/AKfycbxf1CJR2OG2Io90pVLmDqHPc34dj6bicxUQtL48vKVaUlsua2OZZQsoDM7uXpgjQbL5/exec',
+  
   sheetName: 'Costura',
 
   /* Buttons for each person */
