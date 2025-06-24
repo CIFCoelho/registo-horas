61581278fbb3dd79bfba84132af001f925324779
Francisco Coelho <coelho1@ua.pt>
2025-06-24 13:51:33 +0100

    AppsScript URL update for testing new implementation

--- frontend/JS/config/acabamento.config.js
+++ frontend/JS/config/acabamento.config.js
@@ -1,5 +1,5 @@
 window.SECTION_CONFIG = {
   section: 'Acabamento',
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbw42koTxO0pGaYrOs1yN-qgsWdBPRJwo5fzY1yvA7-XouJK0jo3zpT5PJ112lahvv1T/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbzJvZLoonGUQ7T6OD_C1ykhYQrTKvavD6rr4RJy3QSwO-Xt1ZIU_yky5eHhUidE92rb/exec',
   names: ['Cristina', 'Teresa', 'Sónia', 'Inês', 'Diana', 'Antónia']
 };
