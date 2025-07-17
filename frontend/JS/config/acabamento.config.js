5ac08b64eeeadffa2a5c6f570555d3f78a4a3c28
Francisco Coelho <coelho1@ua.pt>
2025-07-17 14:57:10 +0100

    changed 'webAppUrl' to set the endpoint of the backend

--- frontend/JS/config/acabamento.config.js
+++ frontend/JS/config/acabamento.config.js
@@ -1,5 +1,5 @@
 window.SECTION_CONFIG = {
   section: 'Acabamento',
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbzK0rNN3-5jVTQx1oXmU6I7X_0KNI9AxzZI0Kh1HSQe31p54AgwLsiM-YGVkyWNi-Z-/exec',
+  webAppUrl: 'https://www.notion.so/233faf6b5f21805a9919f41735a1590e?v=233faf6b5f21805d9cc7000c0393967b',
   names: ['Antónia', 'Cristina', 'Teresa', 'Pedro']
 };
