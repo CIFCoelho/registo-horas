713bc7fed43df197e8846e0624d758420c634af8
Francisco Coelho <coelho1@ua.pt>
2025-07-17 09:44:08 +0100

    Updated Apps Script URL

--- frontend/JS/config/acabamento.config.js
+++ frontend/JS/config/acabamento.config.js
@@ -1,5 +1,5 @@
 window.SECTION_CONFIG = {
   section: 'Acabamento',
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbzJEnC8CCLqFcnOu2dNFwp3pGF4zC7o4KAWTia551LpYfTWpQOhmZMFMp4Y8lAWYo_W/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbzfiScz0_8B0l6QVnsF8n2P6K7A7np-8hLHMvjjaw0kpyAhCneKvJbSPnfugEqMMuUD/exec',
   names: ['Antónia', 'Cristina', 'Teresa', 'Pedro']
 };
