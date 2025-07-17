800dd278bb5d5bfb131cf999c38b5d2d65b00fb0
Francisco Coelho <coelho1@ua.pt>
2025-07-17 15:17:10 +0100

    Enabled CORS headers in Node.js server so the frontend can post to the backend without browser errors

--- server/index.js
+++ server/index.js
@@ -14,6 +14,14 @@ const headers = {
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.post('/acabamento', async (req, res) => {
   try {
