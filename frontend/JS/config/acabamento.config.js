b5520a6e98dfacc334b22d3cd10d767574594442
Francisco Coelho <coelho1@ua.pt>
2025-07-17 16:32:34 +0100

    Updated worker list to test display size

--- frontend/JS/config/acabamento.config.js
+++ frontend/JS/config/acabamento.config.js
@@ -1,5 +1,5 @@
 window.SECTION_CONFIG = {
   section: 'Acabamento',
   webAppUrl: 'https://www.notion.so/233faf6b5f21805a9919f41735a1590e?v=233faf6b5f21805d9cc7000c0393967b',
-  names: ['Antónia', 'Cristina', 'Teresa', 'Pedro']
+  names: ['Antónia', 'Cristina', 'Diogo', 'Teresa', 'Pedro']
 };
