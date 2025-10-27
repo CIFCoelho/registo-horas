fe2f8bfaffa71ab23201bc0b80dc11f5c479ecef
Francisco Coelho <160111967+coelhof12@users.noreply.github.com>
2025-10-27 10:47:22 +0000

    Add Luana to lixagem roster

--- frontend/JS/config/preparacao.config.js
+++ frontend/JS/config/preparacao.config.js
@@ -1,6 +1,6 @@
 window.SECTION_CONFIG = {
   section: 'Preparação de Madeiras',
   webAppUrl: 'https://registo-horas.onrender.com/preparacao',
-  names: ['Cristina', 'Diogo', 'João', 'Pedro'],
+  names: ['Cristina', 'Diogo', 'João', 'Luana', 'Pedro'],
   storagePrefix: 'preparacaoMadeiras'
 };
